91a9994c1f38d554c82f16adde91f1ea525c282a
box16 <inoinobox16@gmail.com>
2020-10-17 14:50:31 +0900

    結果出力urlの変更

--- mecaber/main/urls.py
+++ mecaber/main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.index,name="index"),
-    path("index_to_noun_result",views.index_to_noun_result,name="index_to_noun_result"),
+    path("noun_result",views.index_to_noun_result,name="index_to_noun_result"),
 ]
